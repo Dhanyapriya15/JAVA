268d45acbcb088306f081b8f6750679bf4fdf1f0
Dhanyapriya15 <180368907+Dhanyapriya15@users.noreply.github.com>
2024-09-04 09:13:59 +0530

    Update reverse.java

--- reverse.java
+++ reverse.java
@@ -1,1 +1,21 @@
-tc ty ctiy  tiy
+package kce;
+
+public class reverse {
+
+	public static void main(String[] args) {
+		int num = 077, reversed = 0;
+	    
+	    System.out.println("Original Number: " + num);
+	    while(num != 0) {
+	    
+	     
+	      int digit = num % 10;
+	      reversed = reversed * 10 + digit;
+
+	      
+	      num /= 10;
+	    }
+
+	    System.out.println("Reversed Number: " + reversed);
+	  }
+	}
